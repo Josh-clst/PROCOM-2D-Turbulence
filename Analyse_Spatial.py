122e7c58579be515164c3769f43882fe0daa47ea
Josh-clst <joshuaicard57@gmail.com>
2025-12-08 16:19:45 +0100

    Bigger scale analysis for the first simulation

--- Analyse_Spatial.py
+++ Analyse_Spatial.py
@@ -27,7 +27,7 @@ import infomeasure as im # to compute information measures
 #%%
 from dirs import dir, save_dir
 
-sim_n = '03'
+sim_n = '01'
 path= dir + 'sim_' + sim_n + '/' + 'vars.nc'
 save_dir = save_dir + 'sim_' + sim_n + '/'
 # Ensure output directory exists to avoid "No such file or directory" when saving
@@ -60,7 +60,7 @@ N=len(WW)
 # Parameters definition
 Nanalyse=2**10 # number of increments to analyse (512 / 1024 is a good compromise between statistical convergence and computation time)
 Nreal=2 # number of realizations for the statistics
-scaleth=40 # maximum scale to analyse
+scaleth=100 # maximum scale to analyse
 
 
 scale_dir = f'scales_1-{scaleth}/'
